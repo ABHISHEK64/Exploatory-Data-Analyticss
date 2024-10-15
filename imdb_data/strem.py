483196b241a1a193f2e5d73e0ad2c8b78ff628ee
Abhishek sachan <54591370+ABHISHEK64@users.noreply.github.com>
2024-10-15 16:48:07 +0530

    Update strem.py
    made some changes

--- imdb_data/strem.py
+++ imdb_data/strem.py
@@ -1,22 +1,36 @@
 import streamlit as st
 import pickle
 import pandas as pd
+import zipfile
+import os
+import io
+import requests
 
 def recommend(movie):
-    movies_index=movies[movies['Title_x']==movie].index[0]
-    movies_list=sorted(list(enumerate(similarity[movies_index])),reverse=True,key= lambda x:x[1])[1:8]
+    movies_index=movie_path[movie_path['Title_x']==movie].index[0]
+    movies_list=sorted(list(enumerate(model_path[movies_index])),reverse=True,key= lambda x:x[1])[1:8]
     recommend_mve=[]
     recommend_images=[]
     for i in movies_list:
-        recommend_mve.append(movies.iloc[i[0]].Title_x)
-        recommend_images.append(movies.iloc[i[0]].image)
+        recommend_mve.append(movie_path.iloc[i[0]].Title_x)
+        recommend_images.append(movie_path.iloc[i[0]].image)
     return recommend_mve,recommend_images
+zip_path = r'https://raw.githubusercontent.com/ABHISHEK64/Exploatory-Data-Analyticss/main/imdb_data/movies.zip'
 
-movie_path=r'C:\Users\abhis\Documents\imdb_data\imdb_data\movies.pkl'
-model_path=r'C:\Users\abhis\Documents\imdb_data\imdb_data\similarity.pkl'
-movies=pickle.load(open(movie_path,'rb'))
-similarity=pickle.load(open(model_path,'rb'))
-movies_list=movies['Title_x'].values
+response = requests.get(zip_path)
+if response.status_code == 200:
+    # Open the ZIP file from the downloaded content in memory
+    with zipfile.ZipFile(io.BytesIO(response.content)) as zip_ref:
+        # Extract the Tweets.pkl file in memory
+        with zip_ref.open('movies.pkl') as pkl_file:
+            movie_path = pickle.load(pkl_file)
+        with zip_ref.open('similarity.pkl')as pkl_file:
+            model_path=pickle.load(pkl_file)
+
+else:
+    st.error("Failed to download the ZIP file from GitHub.")
+
+movies_list=movie_path['Title_x'].values
 st.title("Movies Recomender System")
 
 selected_movies=st.selectbox('Select your favourte movies',(movies_list))
